18b0131bf746ef07fb5e5c88e2a151ee37d35317
paako <bootdev@dsts.info>
2026-09-25 11:37:33 +0000

    Log /account accesses

--- internal/account/handler.go
+++ internal/account/handler.go
@@ -64,6 +64,7 @@ func (handler *Handler) Page(responseWriter http.ResponseWriter, request *http.R
 	if err := handler.renderPage(responseWriter, http.StatusOK, current, ""); err != nil {
 		handler.internalError(responseWriter, request, err)
 	}
+	_ = handler.logger.Event("account_accessed", map[string]any{"userId": current.User.ID, "email": current.User.Email, "expiresAt": formatTimestamp(current.Session.ExpiresAt)})
 }
 
 func (handler *Handler) UpdateEmail(responseWriter http.ResponseWriter, request *http.Request) {
